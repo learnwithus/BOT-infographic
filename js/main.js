4f829daa4d74ce70683a73fb8b29236e0ebec229
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-01-28 16:46:54 -0800

    added data for 3 sites

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
 $(window).on('load', function() {
   console.log('ready');
 
-  // $(window).resize(function() {
-  //   if ($("#infographic-container").height() > $("#background-image").height()) {
-  //     console.log('window taller than it is wide');
-  //     let backgroundHeight = $("#infographic-container").height();
-  //     $("#smoke-svg-1").css("bottom", backgroundHeight / 2);
-  //   }
-  // });
-
   // define objects containing the data for each site
   const vancouverData = {
-    'surgeries': 87002,
-    'births': 12591,
-    'residentialCareClients': 17312,
-    'mentalHealthInpatientVisits': 55555,
-    'homeSupportClients': 15305,
-    'homeSupportVisits': 2951441,
-    'homeCareNursingVisits': 128891,
-    'homeCareRehabVisits': 1234567,
-    'emergencyDepartmentVisits': 460411,
-    'inpatientDays': 596241
+    'surgeries': 31657,
+    'births': 6122,
+    'residentialCareClients': 6838,
+    'psychiatricUnitAdmissions': 2333,
+    'homeSupportClients': 7776,
+    'homeSupportVisits': 1508601,
+    'homeCareNursingVisits': 126734,
+    'homeCareRehabVisits': 31077,
+    'emergencyDepartmentVisits': 119435,
+    'inpatientDays': 282812
+  }
+
+  const richmondData = {
+    'surgeries': 9125,
+    'births': 2089,
+    'residentialCareClients': 1905,
+    'psychiatricUnitAdmissions': 611,
+    'homeSupportClients': 2031,
+    'homeSupportVisits': 316350,
+    'homeCareNursingVisits': 29276,
+    'homeCareRehabVisits': 8620,
+    'emergencyDepartmentVisits': 57676,
+    'inpatientDays': 76243
+  }
+
+  const richmondData = {
+    'surgeries': 23673,
+    'births': 2256,
+    'residentialCareClients': 4653,
+    'psychiatricUnitAdmissions': 1013,
+    'homeSupportClients': 2926,
+    'homeSupportVisits': 587227,
+    'homeCareNursingVisits': 51464,
+    'homeCareRehabVisits': 11670,
+    'emergencyDepartmentVisits': 141665,
+    'inpatientDays': 119561
   }
 
   let countUps = {}; // an object to hold all the CountUp instances
